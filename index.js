076a78c1220440ad9bb5b6d45e0e56a190228426
adventurous-class-0665 <rngao97@gmail.com>
2019-08-29 20:13:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for (var a = 0; a < 4; a++){
+    array[a] = musicians[a] + " plays " + instruments[a];
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var b = 0;
+  while(b < facts.length){
+    facts[b] += "!!!"
+    b++
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number){
+  var emptyArray = []
+  do {
+    emptyArray[number] = "I love the Beatles!"
+    number--
+  } while (0 < number < 15)
+  return emptyArray;
+}
